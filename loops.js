70dd33faee060b27e24d427423b150cb717a5443
Hassan Malik <hbmalik88@gmail.com>
2018-07-28 18:24:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,8 +10,9 @@ function forLoop (array) {
   return array;
 }
 function whileLoop (n) {
-  while (n>0) {
-    console.log(n);
+  let countdown = n;
+  while (countdown>0) {
+    console.log(countdown);
     countdown--;
   }
   if (countdown===0) {

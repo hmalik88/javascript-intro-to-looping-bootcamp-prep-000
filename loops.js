abee8d0fb0dcd227950dfa4e9f98b2f34f6c5f7e
Hassan Malik <hbmalik88@gmail.com>
2018-07-28 17:32:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop (array) {
-  for (i=1; i<26; i++) {
+  for (i=0; i<25; i++) {
     if (i===1) {
     console.log("I am 1 strange loop");
   }
   else {
-    console.log(`I am ${i} strange loop.`);
+    console.log("I am ${i} strange loop.");
   }
   }
 }
